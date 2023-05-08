9bded89a56276c7535e6a485bf2ed4af37203151
Keenwah Ryce <keenwah@admins-iMac.local>
2023-05-08 18:18:27 -0500

    Changed search placeholder value

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,29 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Navbar = ({ searchTerm, setSearchTerm }) => {
-
-
-
-    return (
-        <>  
-        <header>
-            <h1 className='title'><Link to='/'>TRAKSTR</Link></h1>
-            <nav></nav>
-        </header>
-        <div className="search-box">
-            <div className="label-search">
-                <h3 className="btn-united">UNITED</h3>
-                <form action="" className="search"><input type="text" className="input" placeholder="searchTerm" onChange={ (e) => setSearchTerm(e.target.value) } /></form>
-            </div>
+  return (
+    <>
+      <header>
+        <h1 className="title">
+          <Link to="/">TRAKSTR</Link>
+        </h1>
+        <nav></nav>
+      </header>
+      <div className="search-box">
+        <div className="label-search">
+          <h3 className="btn-united">UNITED</h3>
+          <form action="" className="search">
+            <input
+              type="text"
+              className="input"
+              placeholder="search"
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </form>
         </div>
-        </>
-    
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
